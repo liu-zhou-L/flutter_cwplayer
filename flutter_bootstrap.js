17da64d090eeaa936de5f664a1ed6ee0f55579fb
liuzhou <liuzhouzhoulin@gmail.com>
2026-06-02 04:52:51 +0800

    deploy: 370c4ce5 (2026-06-01 20:52 UTC)

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "384642193" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1539009009" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
